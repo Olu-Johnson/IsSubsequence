f66d8fee4e3c6c27138fff6ddaf0186e9b0469d0
Olu-Johnson <olujohnsonoluwatosin@gmail.com>
2023-09-13 03:19:29 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+public class Program
+{
+    public static void Main(string[] args){
+        
+        bool result = IsSubsequence("ace", "abcde");
+        Console.WriteLine(result);
+        Console.ReadLine();
+    }
+
+    public static bool IsSubsequence(string s, string t){
+
+        int i = 0; int j = 0;
+
+        while(i < s.Length && j < t.Length){
+            if(s[i] == t[j]){
+                i++;
+                j++;
+    }
+            else{
+               j++; 
+            }
+        }
+
+        return i == s.Length;
+    }
+}
